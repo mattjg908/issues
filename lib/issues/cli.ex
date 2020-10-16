62ae340df4e89db71dde7db3870c7de40a60d24a
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-16 21:43:30 +0200

    Add Poison dependency * Parse response from Github * Handle fetch errors

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -36,7 +36,7 @@ defmodule Issues.CLI do
   def args_to_internal_representation([user, project]) do
     { user, project, @default_count }
   end
-
+  
   def args_to_internal_representation(_) do # bad arg or --help
     :help
   end
@@ -50,6 +50,14 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
   end
 
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
+  
 end
