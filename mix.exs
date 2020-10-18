71d7da9704099a20f54a4f41e9c4ae2dbb813547
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-18 13:40:41 +0200

    Add excoveralls

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       source_url: "https://github.com/mattjg908/issues",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -25,6 +32,7 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:benchee, "~> 1.0", only: :dev},
+      {:excoveralls, "~> 0.5.5", only: :test},
       {:earmark, "~> 1.4.10"},
       {:ex_doc, "~> 0.20.0"},
       {:httpoison, "~> 1.0.0"},
