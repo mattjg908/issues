b786d244eb31a8333b2932034eb9bfde8fb32739
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-18 14:00:10 +0200

    Add coveralls commands to mix, force them to run in test env Add xref dependency graphs

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Issues.MixProject do
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
