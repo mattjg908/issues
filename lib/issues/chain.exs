c498be304fa2d7f3dd1416e3fbc65f8957c5ec96
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-19 22:48:18 +0200

    Add Chain module for trying out spawnining processes

---
+++ lib/issues/chain.exs
@@ -0,0 +1,29 @@
+defmodule Issues.Chain do
+
+  def counter(prev_pid) do
+    receive do
+      n ->
+        send prev_pid, n + 1
+    end
+  end
+
+  def create_processes(n) do
+    code_to_run = fn (_,send_to) ->
+      spawn(Issues.Chain, :counter, [send_to])
+    end 
+
+    last = Enum.reduce(1..n, self(), code_to_run)
+
+    send(last, 0)    # start the count by sending a zero to the last process
+
+    receive do       # and wait for the result to come back to us
+      final_answer when is_integer(final_answer) ->
+        "Result is #{inspect(final_answer)}"
+    end
+  end
+
+  def run(n) do
+    :timer.tc(Issues.Chain, :create_processes, [n])
+    |> IO.inspect
+  end
+end
