76ca17dd826b9f9af6b07813a74ef76252ef3c2e
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-24 14:13:28 +0200

    Update WorkingWithMultipleProcessesExerciseTests

--- test/working_with_multiple_processes_exercise_test.ex
+++ test/working_with_multiple_processes_exercise_test.ex
@@ -1,33 +1,31 @@
-defmodule WorkingWithMultipleProcessesExerciseTest do
-  use ExUnit.Case          # bring in the test functionality
-  alias WorkingWithMultipleProcessesExercise, as: MultiProcessEx
+defmodule Issues.WorkingWithMultipleProcessesExerciseTest do
+  use ExUnit.Case
+  alias Issues.WorkingWithMultipleProcessesExercise, as: MultiProcessEx
 
+  # trace/3 mentioned here: "https://medium.com/@hoodsuphopeshigh/testing-in-elixir-chapter-4-processes-processes-everywhere-f87ee3281bc"
 
-  describe "run/0" do
-    # TODO, how to test that a function spawns two functions? What I have is not
-    # really testing that
-    test "spawns two processes" do
-      [process1, process2] = MultiProcessEx.run()
-      assert is_process(process1) && is_process(process2)
-    end
+  test "spawn_two_processes/1" do
+    process_count_before =
+      Process.list()
+      |> Enum.count()
 
-    test "sends both processes a unique token" do
-      [process1, process2] = MultiProcessEx.run()
-      # trace/3 mentioned here:
-      # "https://medium.com/@hoodsuphopeshigh/testing-in-elixir-chapter-4-processes-processes-everywhere-f87ee3281bc"
-      :erlang.trace(process1, true [:receive])
-      :erlang.trace(process2, true [:receive])
-      assert_received {:trace, ^process1, :receive, "fred"}
-      assert_received {:trace, ^process2, :receive, "betty"}
-    end
+    MultiProcessEx.spawn_two_processes()
 
-    # TODO, how to test a process sends a message? I have only seen assertions
-    # for receiving so this test also doesn't really test what I want
-    test "both processes send the token back" do
-      MultiProcessEx.run()
-      assert_receive "fred"
-      assert_receive "betty"
-    end
+    process_count_after =
+      Process.list()
+      |> Enum.count()
+
+    assert process_count_before + 2 == process_count_after
+  end
+
+  @tag :pending
+  test "send_token_to_process/2" do
+   assert false
+  end
+
+  @tag :pending
+  test "return_to_sender/1 sends token back to sender" do
+    assert false
   end
 
 end
