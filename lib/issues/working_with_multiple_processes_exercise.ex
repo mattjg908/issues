db88e34ab35e873a0d0e34598b5d1288020d13f7
Matt Galvin <matthewgalvin908@gmail.com>
2020-10-24 14:14:00 +0200

    Make spawn_two_processes/0 tests green

--- lib/issues/working_with_multiple_processes_exercise.ex
+++ lib/issues/working_with_multiple_processes_exercise.ex
@@ -1,4 +1,4 @@
-defmodule WorkingWithMultipleProcessesExercise do
+defmodule Issues.WorkingWithMultipleProcessesExercise do
   @moduledoc """
   Write a program that spawns two processes and then passes each a
   unique token (for example, “fred” and “betty”). Have them send the
@@ -10,6 +10,20 @@ defmodule WorkingWithMultipleProcessesExercise do
   If either answer is no, how could you make it so?
 
   """
+  @doc """
+  Spawns two processes
+  """
+  @spec spawn_two_processes :: [pid()]
+  def spawn_two_processes do
+    parent = self()
+    pid_1 = spawn fn ->
+      send(parent, {:world, self()})
+    end
+    pid_2 = spawn fn ->
+      send(parent, {:world, self()})
+    end
+  end
+
 
   @doc """
   Spawns two processes, sending each one a unique token. The processes then send
